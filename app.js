3104152e07d0704ce1140bf0dc00edf5d4c8903b
Kimleng <kimlenghor2006@gmail.com>
2023-03-13 16:42:23 -0500

    add pug

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const path = require('path');
-const rootDir = require('./util/path')
 
-const adminRoutes = require('./routes/admin');
+const path = require('path');
+const rootDir = require('./util/path');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 //handle 404 pages
 app.use((req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', '404.html'));
+    res.render('404', { pageTitle: 'Page Not Found!'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
-const rootDir = require('../util/path')
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log("In another middleware!");
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', { prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
