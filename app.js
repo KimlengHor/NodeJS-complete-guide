202879df1d6bf664e07d31ac6e3dbb0771747a6c
Kimleng <kimlenghor2006@gmail.com>
2023-03-13 13:26:13 -0500

    add express

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+const bodyParser = require('body-parser');
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    console.log("In another middleware!");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+});
+
+app.post('/product', (req, res, next) => { 
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    console.log("In another middleware!");
+    res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(3000);
+
+
+// const routes = require('./routes');
